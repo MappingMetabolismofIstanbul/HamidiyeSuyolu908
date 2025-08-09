c4857caace5408b3a7152f7a6ab43f2451850b33
MappingMetabolismofIstanbul <dogatmn@gmail.com>
2025-08-09 23:19:06 +0300

    Update config.js

--- config.js
+++ config.js
@@ -60,7 +60,7 @@ var config = {
             description: '',
             location: {
                 center: [28.925, 41.160],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -79,7 +79,7 @@ var config = {
             description: '',
             location: {
                 center: [28.930, 41.151],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -98,7 +98,7 @@ var config = {
             description: '',
             location: {
                 center: [28.945, 41.144],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -117,7 +117,7 @@ var config = {
             description: '',
             location: {
                 center: [28.966, 41.133],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -136,7 +136,7 @@ var config = {
             description: '',
             location: {
                 center: [28.965, 41.118],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -155,7 +155,7 @@ var config = {
             description: '',
             location: {
                 center: [28.971, 41.116],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -174,7 +174,7 @@ var config = {
             description: '',
             location: {
                 center: [28.979, 41.111],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -212,7 +212,7 @@ var config = {
             description: '',
             location: {
                 center: [28.996, 41.101],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -250,7 +250,7 @@ var config = {
             description: '',
             location: {
                 center: [29.008, 41.077],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -269,7 +269,7 @@ var config = {
             description: '',
             location: {
                 center: [29.014, 41.068],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -288,7 +288,7 @@ var config = {
             description: '',
             location: {
                 center: [29.010, 41.059],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -307,7 +307,7 @@ var config = {
             description: '',
             location: {
                 center: [28.998, 41.056],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -326,7 +326,7 @@ var config = {
             description: '',
             location: {
                 center: [28.995, 41.053],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -345,7 +345,7 @@ var config = {
             description: 'II. Abdülhamid Çeşmesi Hamidiye Suyolları’na bağlıdır.',
             location: {
                 center: [28.994, 41.045],
-                zoom: 16,
+                zoom: 14,
                 pitch: 0,
                 bearing: 0
             },
@@ -363,8 +363,8 @@ var config = {
             image: 'https://i.ibb.co/Z1MJT5Mb/MEF-AAP-027-BAP-2025-08-09-13-13-09.png',                
             description: '',                     
             location: {
-                center: [28.987, 41.044],
-                zoom: 16,
+                center: [28.994, 41.045],
+                zoom: 12,
                 pitch: 0,
                 bearing: 0
             },
@@ -382,8 +382,8 @@ var config = {
             image: 'https://i.ibb.co/FLWYYpjD/MEF-AAP-027-BAP-2025-08-09-13-20-23.png',               
             description: '',                     
             location: {
-                center: [28.987, 41.044],
-                zoom: 16,
+                center: [28.994, 41.045],
+                zoom: 12,
                 pitch: 0,
                 bearing: 0
             },
